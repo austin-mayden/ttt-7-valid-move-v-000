f56cc9491b64e00c4eda9d66d880671ce0a2df9c
Austin Mayden <austin.mayden@gmail.com>
2019-08-15 15:08:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    true
+  if board[index] != " "
+    false
   end
 end
